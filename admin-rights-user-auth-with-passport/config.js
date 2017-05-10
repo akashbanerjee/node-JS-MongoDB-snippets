a203c21efb46888f17a808bd817b194b8d337073
Akash Banerjee <akashbnrj07@gmail.com>
2017-05-10 12:02:01 +0530

    Added facebook Oauth authentication for users

--- admin-rights-user-auth-with-passport/config.js
+++ admin-rights-user-auth-with-passport/config.js
@@ -1,4 +1,9 @@
 module.exports = {
   'secretKey' : '12345-67890-09876-54321',
-  'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+  'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+  'facebook': {
+    clientID: '1908798612731558',
+    clientSecret: '59ed3ffd7e396fe10a704711d7f05795',
+    callbackURL: 'https://localhost:3443/users/facebook/callback'
+  }
 }
